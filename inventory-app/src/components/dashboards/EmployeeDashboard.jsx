2366cb8ed818291a79ecd270cd1ecb5f3ebfbac4
wany1ka <markwanyika@gmail.com>
2024-07-21 15:55:59 +0300

    seperate react

--- inventory-app/src/components/dashboards/EmployeeDashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const EmployeeDashboard = () => {
-    return <div>Welcome to the Employee Dashboard</div>;
-};
-
-export default EmployeeDashboard;
--- inventory-app/src/components/dashboards/ManagerDashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ManagerDashboard = () => {
-    return <div>Welcome to the Manager Dashboard</div>;
-};
-
-export default ManagerDashboard;
